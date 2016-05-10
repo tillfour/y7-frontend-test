4638e7e8fd0b69099f48ff96d0e826f515580bb1
Nicholas Tillman <tillfourcreative@gmail.com>
2016-05-10 01:26:16 -0500

    Complete front-end test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('YELLOW7 - Front-end Development Test');
+(function($) {
+  /* jshint expr: true */
+  'use strict';
+var menu = 'close';
+  $('#menu-toggle').click(function(){
+    if(menu === 'close') {
+      $('nav ul').addClass('nav-open');
+      $('nav ul li').fadeIn(500).css('padding', '1em');
+      menu = 'open';
+    } else {
+      $('nav ul li').fadeOut(400);
+      menu = 'close';
+    }
+  });
+}());
